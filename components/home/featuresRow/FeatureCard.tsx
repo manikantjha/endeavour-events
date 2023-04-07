b4749170bef0bf94e4d5eeb19f6c41c481f0684c
manikantjha <being.manikantjha@gmail.com>
2023-04-07 16:49:13 +0530

    Added accent color to more places.

--- components/home/featuresRow/FeatureCard.tsx
+++ components/home/featuresRow/FeatureCard.tsx
@@ -11,7 +11,9 @@ interface IFeatureCard {
 export default function FeatureCard(props: IFeatureCard) {
   return (
     <div className="p-6 bg-white border border-gray-200 rounded-lg text-center">
-      <div className="mx-auto w-fit">{props.objFeature.icon}</div>
+      <div className="mx-auto w-[40px] text-orange-500">
+        {props.objFeature.icon}
+      </div>
       <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900">
         {props.objFeature.title}
       </h5>
--- components/home/figuresRow/FigureCard.tsx
+++ components/home/figuresRow/FigureCard.tsx
@@ -9,7 +9,7 @@ export default function FigureCard(props: IFigrueCard) {
   return (
     <div className="text-center">
       <div className="block p-6 bg-white border border-gray-200 rounded-lg hover:bg-gray-100">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-orange-500">
           {props.objFigrue.figure}
         </h5>
         <p className="font-normal text-gray-700">
